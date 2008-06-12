2a9bd548175986232c85b66905958ef2b0b4c712
Kevin Shay <kshay@sixapart.com>
2008-06-12 14:03:20 +0000

    Safari fix, StarRating tweaks

--- plugins/FieldDay/field_types/StarRating/lib/FieldDay/FieldType/StarRating.pm
+++ plugins/FieldDay/field_types/StarRating/lib/FieldDay/FieldType/StarRating.pm
@@ -35,19 +35,17 @@ sub html_head {
 	return <<"HTML";
 <script type="text/javascript">
 var field_stars = new Array();
-var star_off_url;
-var star_on_url;
-function star_over(field, rating) {
+function star_over(field, rating, on, off) {
 	for (var i = 1; i <= rating; i++) {
 		var star = document.getElementById(field + '_' + i);
-		star.src = star_on_url;
+		star.src = on;
 	}
 	for (var i = rating + 1; i <= field_stars[field]; i++) {
 		var star = document.getElementById(field + '_' + i);
-		star.src = star_off_url;	
+		star.src = off;	
 	}
 }
-function star_out(field, rating) {
+function star_out(field, rating, on, off) {
 	var val = document.getElementById(field).value;
 	if (val) {
 		val = parseInt(val);
@@ -56,11 +54,11 @@ function star_out(field, rating) {
 	}
 	for (var i = 1; i <= val; i++) {
 		var star = document.getElementById(field + '_' + i);
-		star.src = star_on_url;
+		star.src = on;
 	}
 	for (var i = val + 1; i <= field_stars[field]; i++) {
 		var star = document.getElementById(field + '_' + i);
-		star.src = star_off_url;
+		star.src = off;
 	}
 }
 function star_click(field, rating) {
@@ -106,14 +104,18 @@ sub pre_publish {
 	my $class = shift;
 	my ($ctx, $args, $value, $field) = @_;
 	my $name = $field->name . (FieldDay::Template::PubTags::obj_stash_key($ctx, $args) || '');
-	my $stars = $field->data->{'options'}->{'stars'};
-	my $out = qq{<script type="text/javascript">
-field_stars['$name'] = $stars;
+	my $opts = $field->data->{'options'};
+	my $out = '';
+	if ($args->{'enter'}) {
+		$out .= qq{<script type="text/javascript">
+field_stars['$name'] = $opts->{'stars'};
 </script>
 };
-	for my $i (1 .. $stars) {
-		my $src = ($value >= $i) ? $field->data->{'options'}->{'on_url'} : $field->data->{'options'}->{'off_url'};
-		my $attrs = !$args->{'enter'} ? '' : qq{ name="${name}_$i" id="${name}_$i" onmouseover="star_over('$name', $i)" onmouseout="star_out('$name', $i)" onclick="star_click('$name', $i)"};
+	}
+	my $on_off = "'$opts->{'on_url'}', '$opts->{'off_url'}'";
+	for my $i (1 .. $opts->{'stars'}) {
+		my $src = star_url($value, $i, $opts);
+		my $attrs = !$args->{'enter'} ? '' : qq{ name="${name}_$i" id="${name}_$i" onmouseover="star_over('$name', $i, $on_off)" onmouseout="star_out('$name', $i, $on_off)" onclick="star_click('$name', $i, $on_off)"};
 		$out .= qq{<img src="$src"$attrs />};
 		if ($args->{'enter'}) {
 			$out .= qq{<input type="hidden" name="$name" id="$name" value="$value" />};
