8ec5d3377c6b7882178cd988b11613271301d339
Kevin Shay <kshay@sixapart.com>
2008-07-12 15:37:07 +0000

    convert_breaks for TextAreas

--- plugins/FieldDay/field_types/TextArea/lib/FieldDay/FieldType/TextArea.pm
+++ plugins/FieldDay/field_types/TextArea/lib/FieldDay/FieldType/TextArea.pm
@@ -1,6 +1,7 @@
 
 package FieldDay::FieldType::TextArea;
 use strict;
+use Data::Dumper;
 
 use base qw( FieldDay::FieldType );
 
@@ -23,6 +24,24 @@ sub pre_render {
 	if ($param->{'width'}) {
 		$param->{'wrapper_width'} = $param->{'width'} + 20;
 	}
+}
+
+sub pre_publish {
+	my $class = shift;
+	my ($ctx, $args, $value, $field) = @_;
+	return $value unless ($field->object_type eq 'entry');
+	my $entry = $ctx->stash('entry');
+	return $value unless $entry;
+    my $convert_breaks = exists $args->{convert_breaks} ?
+        $args->{convert_breaks} :
+            defined $entry->convert_breaks ? $entry->convert_breaks :
+                $ctx->stash('blog')->convert_paras;
+    if ($convert_breaks) {
+        my $filters = $entry->text_filters;
+        push @$filters, '__default__' unless @$filters;
+        $value = MT->apply_text_filters($value, $filters, $_[0]);
+    }
+    return $value;
 }
 
 1;
