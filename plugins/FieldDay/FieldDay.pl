049d797b6ac3a1ceabbb9eec86580c5865f58a06
Kevin Shay <kshay@sixapart.com>
2008-08-11 21:18:17 +0000

    "Clear" for autocomplete menu

--- plugins/FieldDay/FieldDay.pl
+++ plugins/FieldDay/FieldDay.pl
@@ -4,7 +4,7 @@ use strict;
 use Data::Dumper;
 
 use vars qw( $VERSION $SCHEMA_VERSION );
-$VERSION = '1.1.4blogscom';
+$VERSION = '1.1.5blogscom';
 $SCHEMA_VERSION = '0.1594';
 
 use base qw( MT::Plugin );
