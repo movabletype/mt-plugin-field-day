ef55e170328c48a5c682767007f7ae2b3e28d77a
Kevin Shay <kshay@sixapart.com>
2009-01-07 17:43:01 +0000

    Bumping version

--- plugins/FieldDay/FieldDay.pl
+++ plugins/FieldDay/FieldDay.pl
@@ -4,7 +4,7 @@ use strict;
 use Data::Dumper;
 
 use vars qw( $VERSION $SCHEMA_VERSION );
-$VERSION = '1.2.9goat';
+$VERSION = '1.2.10goat';
 $SCHEMA_VERSION = '0.1594';
 
 use base qw( MT::Plugin );
